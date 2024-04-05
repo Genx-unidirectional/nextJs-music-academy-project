b13599c7f0033f3ebf82750800155a3e9dff10c5
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-05 14:52:05 +0530

    feat navbar with aceternity ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">Chai Aur Code</h1>
+    </main>
+  );
 }
